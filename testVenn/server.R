a63ef9f85a19be0facc8c0b48ae47015006525b9
jumingxing <jumingxing@puhuifinance.com>
2016-09-29 11:55:23 +0800

    update

--- testVenn/server.R
+++ testVenn/server.R
@@ -1,5 +1,4 @@
 library(shiny)
-library(gplots)
 library(VennDiagram)
 oneName = function() paste(sample(LETTERS,5,replace=TRUE),collapse="")
 geneNames = replicate(1000, oneName())
@@ -10,15 +9,17 @@ C = sample(geneNames, 250, replace=FALSE)
 function(input,output){
   dataSet <- reactive({
     switch(input$dataGroup,
-           "all" = list(A,B,C),
-           "AB" = list(A,B),
-           "AC" = list(A,C),
-           "BC" = list(B,C))
+           "all" = list(A = A,B = B,C = C),
+           "AB" = list(A = A,B = B),
+           "AC" = list(A = A,C = C),
+           "BC" = list(B = B,C = C))
   })
   output$vennPlot <-renderPlot({
     # venn(dataSet(),intersections = T)
-    T<-venn.diagram(dataSet(),filename=NULL
-                    ,rotation.degree=180)
+    T<-venn.diagram(dataSet(),filename=NULL,
+                    col = 1:length(dataSet()),fill = 1:length(dataSet()),
+                    cat.col = 1:length(dataSet()),cat.pos = 2,
+                    rotation.degree=180)
     grid.draw(T)
   })
 }
