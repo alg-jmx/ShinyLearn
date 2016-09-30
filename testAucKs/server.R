86738561a08a1ebcf0396b7448a964a2edf69054
jumingxing <jumingxing@puhuifinance.com>
2016-09-30 11:38:14 +0800

    update

--- testAucKs/server.R
+++ testAucKs/server.R
@@ -12,7 +12,11 @@ function(input, output, session){
   # 
   # Return:
   #   返回一个可交互的web页面
-  dataLabel  = reactive({  #
+  dataLabel  = reactive({  # reactive限定在render*函数体外使用input输入数据
+    # 获取预测数据+真实label
+    #
+    # Return:
+    #   返回一个函数体
     path1 = input$file1
     path2 = input$file2
     if(is.null(path1)|is.null(path2)){
@@ -26,29 +30,38 @@ function(input, output, session){
     dataLabel
   })
   output$'AUC-KS' <- renderPlot(
+    # 输出AUC-KS图
     {
       if(is.null(dataLabel())){
         return(NULL)
       }
-      ksRsult = KsValue(dataLabel(),input$group)
+      ksRsult = KsValue(dataLabel(),input$group)  # 计算KS值函数
       ksValue = abs(ksRsult[,1] - ksRsult[,2])
       index = which.max(ksValue)
       value = round(ksValue[index],4)
-      par(mfrow = c(1,2))
+      par(mfrow = c(1,2))  # 限定图形是一行两列
       roc(dataLabel()[,2], as.vector(dataLabel()[,1]), auc = T,plot = T,print.auc=T)
       plot(ksRsult[,1],type = "l",col = 3,main = "KS plot")
       lines(ksRsult[,2],col = 2)
-      segments(index,ksRsult[,2][index],index,ksRsult[,1][index],col = "blue")
+      segments(index,ksRsult[,2][index],index,ksRsult[,1][index],col = "blue")  # 添加KS值区间线段
       text((input$group+4)/2,0.4,paste("ksValue = ",value))
       par(mfrow = c(1,1))
     }
   )
   output$currentTime <- renderText({
+    # 输出字符串
+    # 
+    # Return:
+    #   返回系统当前时间
     invalidateLater(1000, session)
     paste("The current time is", Sys.time())
   })
   
   output$summary <- renderPrint({
+    # 任何打印输出
+    # 
+    # Return:
+    #   返回数据集的统计信息
     if(is.null(dataLabel())){
       return(NULL)
     }else{
@@ -57,10 +70,18 @@ function(input, output, session){
   })
   
   output$view <- renderTable({
+    # 输出数据框、矩阵以及其他表格形式
+    # 
+    # Return:
+    #   返回数据集的前六列
     head(dataLabel())
   })
   
   output$downloadData <- downloadHandler(
+    # 下载数据集
+    # 
+    # Return:
+    #   无返回值，直接给出路径选择弹窗
     filename = function() { 
       paste("dataLabel", '.csv', sep='') 
     },
--- testAucKs/ui.R
+++ testAucKs/ui.R
@@ -2,11 +2,11 @@ library(shiny)
 
 shinyUI(
   fluidPage(
-    titlePanel("model AUC_KS value plot"),
-    sidebarLayout(
-      sidebarPanel(
-        textOutput("currentTime"),
-        fileInput('file1', 'Choose One CSV File',
+    titlePanel("model AUC_KS value plot"),  # 设置UI名称
+    sidebarLayout(  # 页面分层
+      sidebarPanel(  # 辅助面板（主要包含各种控件）
+        textOutput("currentTime"),  # 定义输出文本
+        fileInput('file1', 'Choose One CSV File',  # 定义路径选择
                   accept=c('text/csv', 
                            'text/comma-separated-values,text/plain', 
                            '.csv')),
@@ -15,8 +15,8 @@ shinyUI(
                            'text/comma-separated-values,text/plain', 
                            '.csv')),
         tags$hr(),
-        checkboxInput('header', 'Header', TRUE),
-        radioButtons('sep', 'Separator',
+        checkboxInput('header', 'Header', TRUE), # 单选控件
+        radioButtons('sep', 'Separator',  # 多选控件
                      c(Comma=',',
                        Semicolon=';',
                        Tab='\t'),
@@ -26,21 +26,21 @@ shinyUI(
                        'Double Quote'='"',
                        'Single Quote'="'"),
                      '"'),
+        br(),  # 换行
+        sliderInput("group", "groups to cut", min = 10,max = 50,value = 20),  # 滑条选择控件
         br(),
-        sliderInput("group", "groups to cut", min = 10,max = 50,value = 20),
+        submitButton("Update View"),  # 更新提交控件
         br(),
-        submitButton("Update View"),
-        br(),
-        downloadButton('downloadData', 'Download')
+        downloadButton('downloadData', 'Download')  # 下载控件
       ),
       
-      mainPanel(
-        h4("AUC-KS Plot"),
-        plotOutput("AUC-KS"),
+      mainPanel(  # 主面板
+        h4("AUC-KS Plot"),  # h4字体
+        plotOutput("AUC-KS"),  # 定义输出图
         h4("summary"),
-        verbatimTextOutput("summary"),
+        verbatimTextOutput("summary"),  # 逐字输出文本
         h4("前6行"),
-        tableOutput("view")
+        tableOutput("view")  # 表格输出
       )
     )
   )
